334b8b631bec48f60f6493965bd255a0db282a07
SturdyBen <116659416+benslife12@users.noreply.github.com>
2024-09-10 11:49:35 -0400

    added screenshot function
    double :3

--- Worldwide/WW/WW.py
+++ Worldwide/WW/WW.py
@@ -9,11 +9,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import requests
-
 #rng rng rng
+import time
 import random
 
-
+path = "../output/output.png"
 
 #configuring the chrome window, change the executable path to the path of your chrome driver
 service = Service(executable_path=r"../cdriver/chromedriver.exe")
@@ -22,9 +22,15 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--log-level=3") #clears all the clutter 
 chrome_options.add_argument('--ignore-certificate-errors') #just in case necessary
 chrome_options.add_argument('--allow-running-insecure-content') #just in case necessary
+chrome_options.add_argument("window-size=1920,1080")
 browser = webdriver.Chrome(service=service,options=chrome_options)
 
 #list of words for the bot to google
-words = ["swag","ohio","osama","ramen","capybara therapy"]
+words = ["youtube.com", "google.com"]
 
-browser.get(f"https://www.google.com/search?q={random.choice(words)}")
+browser.get(f"https://{random.choice(words)}")
+
+browser.save_screenshot(path)
+
+#plants = driver.find_elements(By.TAG_NAME, "body")
+#print(plants)
